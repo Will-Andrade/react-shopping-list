afb682e0d14f4a45ca327b0e0e66684ed7b30cfe
Will-Andrade <willian.santos.andra@outlook.com>
2023-02-16 22:38:43 -0400

    feat: project completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FormEvent, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(): JSX.Element {
+  const [items, setItems] = useState<string[]>([]);
+
+  const onSubmitHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const inputValue: string = e.currentTarget.newItem.value;
+    
+    if (inputValue.trim().length > 0) {
+      setItems([...items, inputValue]);
+    }
+  };
+
+  const onDeleteHandler = (itemData: string) => {
+    setItems(items.filter(item => item !== itemData));
+  }
+
+  return (<>
+    <h1>Shopping List</h1>
+
+    <section>
+      <h2>Items to buy</h2>
+
+      <form onSubmit={onSubmitHandler}>
+        <input 
+          type="text" 
+          name="newItem" 
+          placeholder="Add a new item" 
+          required 
+        />
+        <button type="submit">Add</button>
+      </form>
+
+      {items.length > 0 &&
+        <ul>
+          {items.map((item, index) => (
+            <li key={`${index}-${item}`}>
+              {item}
+              <button type="button" onClick={() => onDeleteHandler(item)}>x</button>
+            </li>
+          ))}
+        </ul>
+      }
+    </section>
+  </>);
 }
 
 export default App;
